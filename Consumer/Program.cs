a9b064cb76760c8e0090b5a129c2b4ccaca5e2dd
Furkan <esquetta@gmail.com>
2024-03-06 21:44:54 +0300

    Add project files.

---
+++ Consumer/Program.cs
@@ -0,0 +1,43 @@
+﻿using MassTransit;
+using Shared;
+
+namespace Procuder
+{
+
+    class Message : IMessage
+    {
+        public string Text { get; set; }
+    }
+    class MessageConsumer : IConsumer<IMessage>
+    {
+        public async Task Consume(ConsumeContext<IMessage> context) => Console.WriteLine($"Gelen mesaj : {context.Message.Text}");
+    }
+    class Program
+    {
+
+        static async Task Main(string[] args)
+        {
+            string rabbitMqUri = "amqps://bfqvojol:pQWSjSSPlDBCjGzCdWRc-fhk9qD_aeO8@cow.rmq2.cloudamqp.com/bfqvojol";
+            string queue = "test-queue";
+            string userName = "bfqvojol";
+            string password = "pQWSjSSPlDBCjGzCdWRc-fhk9qD_aeO8";
+
+            var bus = Bus.Factory.CreateUsingRabbitMq(factory =>
+            {
+                factory.Host(rabbitMqUri, config =>
+                {
+                    config.Username(userName);
+                    config.Password(password);
+                });
+
+                factory.ReceiveEndpoint(queue, endpoint => endpoint.Consumer<MessageConsumer>());
+            });
+
+            await bus.StartAsync();
+            Console.ReadLine();
+            await bus.StopAsync();
+
+
+        }
+    }
+}
