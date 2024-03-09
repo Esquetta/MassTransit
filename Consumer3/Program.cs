999e13570a6c53d87bdcf3149a5cf4748c931d05
Furkan <esquetta@gmail.com>
2024-03-09 19:32:54 +0300

    Publish example

---
+++ Consumer3/Program.cs
@@ -0,0 +1,45 @@
+﻿
+using MassTransit;
+using Shared;
+
+namespace Consumer1
+{
+    class Message : IMessage
+    {
+        public string Text { get; set; }
+    }
+    class MessageConsumer : IConsumer<IMessage>
+    {
+        public async Task Consume(ConsumeContext<IMessage> context) => Console.WriteLine($"test-queue-3 Gelen mesaj : {context.Message.Text}");
+    }
+
+    class Program
+    {
+
+
+        static async Task Main()
+        {
+            string rabbitMqUri = "amqps://bfqvojol:pQWSjSSPlDBCjGzCdWRc-fhk9qD_aeO8@cow.rmq2.cloudamqp.com/bfqvojol";
+            string queue = "test-queue3";
+            string userName = "bfqvojol";
+            string password = "pQWSjSSPlDBCjGzCdWRc-fhk9qD_aeO8";
+
+
+            var bus = Bus.Factory.CreateUsingRabbitMq(factory =>
+            {
+                factory.Host(rabbitMqUri, conf =>
+                {
+                    conf.Username(userName);
+                    conf.Password(password);
+                });
+
+                factory.ReceiveEndpoint(queue, configureEndPoint => configureEndPoint.Consumer<MessageConsumer>());
+            });
+
+            await bus.StartAsync();
+            Console.ReadLine();
+            await bus.StopAsync();
+
+        }
+    }
+}
---
+++ PublishProducer/Program.cs
@@ -0,0 +1,50 @@
+﻿using MassTransit;
+using Shared;
+
+namespace Consumer1
+{
+    class Message : IMessage
+    {
+        public string Text { get; set; }
+    }
+
+    class Program
+    {
+
+
+        static async Task Main()
+        {
+            string rabbitMqUri = "amqps://bfqvojol:pQWSjSSPlDBCjGzCdWRc-fhk9qD_aeO8@cow.rmq2.cloudamqp.com/bfqvojol";
+            string queue = "test-queue1";
+            string userName = "bfqvojol";
+            string password = "pQWSjSSPlDBCjGzCdWRc-fhk9qD_aeO8";
+
+
+            var bus = Bus.Factory.CreateUsingRabbitMq(factory =>
+            {
+                factory.Host(rabbitMqUri, config =>
+                {
+                    config.Username(userName);
+                    config.Password(password);
+                });
+            });
+
+            await Task.Run(async () =>
+            {
+                while (true)
+                {
+                    Console.Write("Mesaj yaz : ");
+                    Message message = new Message
+                    {
+                        Text = Console.ReadLine()
+                    };
+                    if (message.Text.ToUpper() == "C")
+                        break;
+                    await bus.Publish<IMessage>(message);
+                    Console.WriteLine("");
+                }
+            });
+
+        }
+    }
+}
